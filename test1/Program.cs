47aa1d3a4c2ff70a8a81e48f92b6217a3e034be2
Yang <113082249+1234567Yang@users.noreply.github.com>
2023-06-23 19:33:45 +0000

    2

--- test1/Program.cs
+++ test1/Program.cs
@@ -7,12 +7,25 @@ using System.Threading;
 class Program {
     static void Main(string[] args) {
         Console.WriteLine("Starting");
-        HT ht = new HT();
-        Console.WriteLine(ht.get("https://example.com/"));
+        HTTPDodwnload ht = new HTTPDodwnload("https://example.com/");
+
+        //Console.WriteLine(ht.changeDir("/e/1.txt"));
+        //Console.WriteLine(ht.getFile());
     }
 
-    class HT{
-    public string get(string url){
+    class HTTPDodwnload{
+        private string url = "";
+        public HTTPDodwnload(string url){
+            //Console.WriteLine(url.Substring(url.Length - 1,1));
+            if(url.Substring(url.Length - 1,1) == "/"){
+                this.url = url;
+            }else{
+                this.url = url + "/";
+            }
+            Console.WriteLine(this.url);
+        }
+
+    public string getFile(){
         using var client = new HttpClient();
     
         // 发送 GET 请求以获取指定 URL 的响应
@@ -23,6 +36,14 @@ class Program {
     
         // 输出响应内容
         return responseBody;
+    }
+
+    public string changeDir(string relativePath){
+        Uri baseUri = new Uri(url);
+        Uri absoluteUri = new Uri(baseUri, relativePath);
+        string absolutePath = absoluteUri.AbsoluteUri;
+        return absolutePath;
+
     }
     }
 }
