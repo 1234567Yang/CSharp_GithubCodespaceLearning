89da40c138313c0700e8691a0399525a0cc813b9
Yang <113082249+1234567Yang@users.noreply.github.com>
2023-09-27 23:06:49 +0000

    add stack usage

--- test2/Program.cs
+++ test2/Program.cs
@@ -14,22 +14,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
         
             image1 = new Bitmap(@"test.png", true);
 
-        int x, y;
+            MemoryStream stream = new MemoryStream();
+            image1.Save(stream, ImageFormat.Png);
 
-        // Loop through the images pixels to reset color.
-        for(x=0; x<image1.Width; x++)
-        {
-            for(y=0; y < image1.Height; y++)
-            {
-                Color pixelColor = image1.GetPixel(x, y);
-                Color newColor = Color.FromArgb((pixelColor.R - 50 > 0) ? (pixelColor.R - 50 + rd.Next(0,50)) : (pixelColor.R), (pixelColor.G - 50 > 0) ? (pixelColor.G - 50 + rd.Next(0,50)) : (pixelColor.G), (pixelColor.B - 50 > 0) ? (pixelColor.B - 50 + rd.Next(0,50)) : (pixelColor.B));
-                image1.SetPixel(x, y, newColor);
-            }
-        }
-        MemoryStream stream = new MemoryStream();
-        image1.Save(stream, ImageFormat.Png);
-
-        using (FileStream file = new FileStream(@"output.png", FileMode.OpenOrCreate, FileAccess.Write))
+            using (FileStream file = new FileStream(@"output.png", FileMode.OpenOrCreate, FileAccess.Write))
             {
                 stream.WriteTo(file);
             }
---
+++ test3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(66666);
+            stack.Push("vevd");
+            // stack.Add(533);
+
+            stack.Pop();
+            Console.WriteLine(stack.Peek());
+
+            foreach(var i in stack){
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("***End***");
+        }
+    }
+}
